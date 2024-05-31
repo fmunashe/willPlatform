17a7f0e86459b5276cabf087fe7ce734acb03670
farai.zihove <farai.zihove@econet.co.zw>
2024-05-31 19:04:51 +0200

    update mappers

--- will-platform-core/src/main/java/zw/co/zim/willplatform/service/WillBurialRequestService.java
+++ will-platform-core/src/main/java/zw/co/zim/willplatform/service/WillBurialRequestService.java
@@ -1,13 +1,13 @@
 package zw.co.zim.willplatform.service;
 
 import org.springframework.data.domain.Page;
-import zw.co.zim.willplatform.common.AppService;
+import zw.co.zim.willplatform.common.BaseService;
 import zw.co.zim.willplatform.model.Client;
 import zw.co.zim.willplatform.model.WillBurialRequest;
 
 import java.util.Optional;
 
-public interface WillBurialRequestService extends AppService<WillBurialRequest> {
+public interface WillBurialRequestService extends BaseService<WillBurialRequest> {
     Page<WillBurialRequest> findAll(Integer pageNo, Integer pageSize);
 
     Optional<WillBurialRequest> findFirstByUserId(Client userId);
