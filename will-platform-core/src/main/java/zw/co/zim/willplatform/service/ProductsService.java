58d7abf8fc0866a500e1475eb12f95a7032e5a7d
farai.zihove <farai.zihove@econet.co.zw>
2024-05-31 19:04:15 +0200

    update mappers

--- will-platform-core/src/main/java/zw/co/zim/willplatform/service/ProductsService.java
+++ will-platform-core/src/main/java/zw/co/zim/willplatform/service/ProductsService.java
@@ -1,14 +1,14 @@
 package zw.co.zim.willplatform.service;
 
 import org.springframework.data.domain.Page;
-import zw.co.zim.willplatform.common.AppService;
+import zw.co.zim.willplatform.common.BaseService;
 import zw.co.zim.willplatform.utils.enums.ProductNames;
 import zw.co.zim.willplatform.model.Currency;
 import zw.co.zim.willplatform.model.Products;
 
 import java.util.Optional;
 
-public interface ProductsService extends AppService<Products> {
+public interface ProductsService extends BaseService<Products> {
     Page<Products> findAll(Integer pageNo,Integer pageSize);
 
     Page<Products> findAllByCurrency( Currency currency,Integer pageNo,Integer pageSize);
--- will-platform-core/src/main/java/zw/co/zim/willplatform/utils/messages/request/OutstandingAccountRequest.java
+++ will-platform-core/src/main/java/zw/co/zim/willplatform/utils/messages/request/OutstandingAccountRequest.java
@@ -1,5 +1,7 @@
 package zw.co.zim.willplatform.utils.messages.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,7 +12,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class OutstandingAccountRequest {
+    @NotNull(message = "Account name is required")
+    @NotBlank(message = "Account name cannot be blank")
     private String nameOfAccount;
+    @NotNull(message = "Account value is required")
     private Double accountValue;
+    @NotNull(message = "Client id is required")
     private Long clientId;
 }
