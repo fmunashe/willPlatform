5cee2b40e82640ddc8c24bb8fce1ca549b20db29
farai.zihove <farai.zihove@econet.co.zw>
2024-05-31 19:06:12 +0200

    create new dtos

---
+++ will-platform-core/src/main/java/zw/co/zim/willplatform/dto/LiabilitiesSubscribedWrittenPublicationDto.java
@@ -0,0 +1,18 @@
+package zw.co.zim.willplatform.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import zw.co.zim.willplatform.model.Client;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class LiabilitiesSubscribedWrittenPublicationDto extends BaseDto {
+    private String nameOfPublication;
+    private Client userId;
+}
