eac45b5f84ab02e36f2267eb2d978bcb172ca23d
farai.zihove <farai.zihove@econet.co.zw>
2024-05-31 19:04:10 +0200

    update mappers

--- will-platform-core/src/main/java/zw/co/zim/willplatform/service/LiabilitiesSubscribedWrittenPublicationService.java
+++ will-platform-core/src/main/java/zw/co/zim/willplatform/service/LiabilitiesSubscribedWrittenPublicationService.java
@@ -1,11 +1,11 @@
 package zw.co.zim.willplatform.service;
 
 import org.springframework.data.domain.Page;
-import zw.co.zim.willplatform.common.AppService;
+import zw.co.zim.willplatform.common.BaseService;
 import zw.co.zim.willplatform.model.Client;
 import zw.co.zim.willplatform.model.LiabilitiesSubscribedWrittenPublication;
 
-public interface LiabilitiesSubscribedWrittenPublicationService extends AppService<LiabilitiesSubscribedWrittenPublication> {
+public interface LiabilitiesSubscribedWrittenPublicationService extends BaseService<LiabilitiesSubscribedWrittenPublication> {
     Page<LiabilitiesSubscribedWrittenPublication> findAll(Integer pageNo, Integer pageSize);
 
     Page<LiabilitiesSubscribedWrittenPublication> findAllByUserId(Client clientId, Integer pageNo, Integer pageSize);
--- will-platform-core/src/main/java/zw/co/zim/willplatform/service/LiabilitiesUserOweMoneyService.java
+++ will-platform-core/src/main/java/zw/co/zim/willplatform/service/LiabilitiesUserOweMoneyService.java
@@ -1,11 +1,11 @@
 package zw.co.zim.willplatform.service;
 
 import org.springframework.data.domain.Page;
-import zw.co.zim.willplatform.common.AppService;
+import zw.co.zim.willplatform.common.BaseService;
 import zw.co.zim.willplatform.model.Client;
 import zw.co.zim.willplatform.model.LiabilitiesUserOwesMoney;
 
-public interface LiabilitiesUserOweMoneyService extends AppService<LiabilitiesUserOwesMoney> {
+public interface LiabilitiesUserOweMoneyService extends BaseService<LiabilitiesUserOwesMoney> {
     Page<LiabilitiesUserOwesMoney> findAll(Integer pageNo, Integer pageSize);
 
     Page<LiabilitiesUserOwesMoney> findAllByUserId(Client clientId, Integer pageNo, Integer pageSize);
