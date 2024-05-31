c744f0ed2d1756f28f3b9229cea50c4e1f29610d
farai.zihove <farai.zihove@econet.co.zw>
2024-05-31 19:04:55 +0200

    update mappers

--- will-platform-core/src/main/java/zw/co/zim/willplatform/service/WillDocumentsService.java
+++ will-platform-core/src/main/java/zw/co/zim/willplatform/service/WillDocumentsService.java
@@ -1,11 +1,11 @@
 package zw.co.zim.willplatform.service;
 
 import org.springframework.data.domain.Page;
-import zw.co.zim.willplatform.common.AppService;
+import zw.co.zim.willplatform.common.BaseService;
 import zw.co.zim.willplatform.model.Client;
 import zw.co.zim.willplatform.model.WillDocuments;
 
-public interface WillDocumentsService extends AppService<WillDocuments> {
+public interface WillDocumentsService extends BaseService<WillDocuments> {
     Page<WillDocuments> findAll(Integer pageNo, Integer pageSize);
 
     Page<WillDocuments> findAllByUserId(Client userId, Integer pageNo, Integer pageSize);
--- will-platform-core/src/main/java/zw/co/zim/willplatform/service/WillPasswordsService.java
+++ will-platform-core/src/main/java/zw/co/zim/willplatform/service/WillPasswordsService.java
@@ -1,11 +1,11 @@
 package zw.co.zim.willplatform.service;
 
 import org.springframework.data.domain.Page;
-import zw.co.zim.willplatform.common.AppService;
+import zw.co.zim.willplatform.common.BaseService;
 import zw.co.zim.willplatform.model.Client;
 import zw.co.zim.willplatform.model.WillPasswordsInstructions;
 
-public interface WillPasswordsService extends AppService<WillPasswordsInstructions> {
+public interface WillPasswordsService extends BaseService<WillPasswordsInstructions> {
     Page<WillPasswordsInstructions> findAll(Integer pageNo, Integer pageSize);
 
     Page<WillPasswordsInstructions> findAllByUserId(Client userId, Integer pageNo, Integer pageSize);
