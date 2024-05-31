c1cc78177315d87c4466a5251dd7d980f02ece62
farai.zihove <farai.zihove@econet.co.zw>
2024-05-31 19:04:22 +0200

    update mappers

--- will-platform-core/src/main/java/zw/co/zim/willplatform/service/PropertyAssetService.java
+++ will-platform-core/src/main/java/zw/co/zim/willplatform/service/PropertyAssetService.java
@@ -1,11 +1,11 @@
 package zw.co.zim.willplatform.service;
 
 import org.springframework.data.domain.Page;
-import zw.co.zim.willplatform.common.AppService;
+import zw.co.zim.willplatform.common.BaseService;
 import zw.co.zim.willplatform.model.Client;
 import zw.co.zim.willplatform.model.PropertyAsset;
 
-public interface PropertyAssetService extends AppService<PropertyAsset> {
+public interface PropertyAssetService extends BaseService<PropertyAsset> {
     Page<PropertyAsset> findAll(Integer pageNo, Integer pageSize);
 
     Page<PropertyAsset> findAllByUserId(Client clientId, Integer pageNo, Integer pageSize);
--- will-platform-core/src/main/java/zw/co/zim/willplatform/service/QuestionnaireService.java
+++ will-platform-core/src/main/java/zw/co/zim/willplatform/service/QuestionnaireService.java
@@ -1,13 +1,13 @@
 package zw.co.zim.willplatform.service;
 
 import org.springframework.data.domain.Page;
-import zw.co.zim.willplatform.common.AppService;
+import zw.co.zim.willplatform.common.BaseService;
 import zw.co.zim.willplatform.model.Client;
 import zw.co.zim.willplatform.model.Questionnaire;
 
 import java.util.Optional;
 
-public interface QuestionnaireService extends AppService<Questionnaire> {
+public interface QuestionnaireService extends BaseService<Questionnaire> {
     Page<Questionnaire> findAll(Integer pageNo, Integer pageSize);
 
     Optional<Questionnaire> findFirstByUserId(Client userId);
