5558a22c10e4230a53f9ceb7eca1092c2f3d301e
farai.zihove <farai.zihove@econet.co.zw>
2024-05-31 19:04:27 +0200

    update mappers

--- will-platform-core/src/main/java/zw/co/zim/willplatform/service/SignupProgressJourneyService.java
+++ will-platform-core/src/main/java/zw/co/zim/willplatform/service/SignupProgressJourneyService.java
@@ -1,13 +1,13 @@
 package zw.co.zim.willplatform.service;
 
 import org.springframework.data.domain.Page;
-import zw.co.zim.willplatform.common.AppService;
+import zw.co.zim.willplatform.common.BaseService;
 import zw.co.zim.willplatform.model.Client;
 import zw.co.zim.willplatform.model.SignupProgressJourney;
 
 import java.util.Optional;
 
-public interface SignupProgressJourneyService extends AppService<SignupProgressJourney> {
+public interface SignupProgressJourneyService extends BaseService<SignupProgressJourney> {
     Page<SignupProgressJourney> findAll(Integer pageNo, Integer pageSize);
     Optional<SignupProgressJourney> findFirstByUserId(Client userId);
 }
